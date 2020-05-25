9ebf7dd841c88a66a879fc6152a1349b2760f357
1395505 <1395505@students.wits.ac.za>
2020-05-25 14:55:06 +0200

    PHP FILES

---
+++ FacultyRegistrationTest_2/getCourses.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$conn = mysqli_connect("127.0.0.1","s1422085","s1422085","d1422085") or die("Connection not established".$conn->connect_error) ;
+// Check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM SD_COURSES WHERE SCHOOL_ID='CSAM'";
+$result = mysqli_query($conn,$sql);
+
+$json = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		$json[]= $row;
+    }
+
+    echo json_encode($json);
+
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
+?>
